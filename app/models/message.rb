b3241f9c95fdbd80a9af696538668ce6c6504784
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-04-10 00:02:04 -0400

    Update Readme.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
+	belongs_to :language
 	belongs_to :sender, :class_name => 'User'
 	belongs_to :recipient, :class_name => 'User'
-	belongs_to :language
 
 	def self.levels_within_range?(message)
 		language = message.language
